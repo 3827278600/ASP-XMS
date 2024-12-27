e9cd2a113b9d7f38890942462ae8e8befa9ba0be
huangyafeng <v-huangyafeng1@xiaomi.com>
2024-12-28 05:50:18 +0800

    feat: asdad

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import progress from 'vite-plugin-progress';
+// import { createHtmlPlugin } from 'vite-plugin-html';
+import legacy from '@vitejs/plugin-legacy';
 
 declare global {
   interface Window {
@@ -8,22 +11,29 @@ declare global {
   }
 }
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   const isQiankun = process.env.QIANKUN || false;
-  console.log(command, mode);
-  
-  
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log(env, 'env');
+
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      progress(),
+      legacy({
+        targets: ['defaults', 'not IE 11'],
+      }),
+    ],
     server: {
       port: 8000,
       open: true,
       host: '0.0.0.0',
       strictPort: false,
       cors: true,
+      strict: false,
       proxy: {
         '/api': {
-          target: 'http://localhost:8000',
+          target: env.VITE_APP_BASE_API,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
@@ -32,8 +42,46 @@ export default defineConfig(({ command, mode }) => {
     base: isQiankun ? '/sub-app/' : '/',
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "src"),
+        '@': path.resolve(__dirname, 'src'),
       },
     },
-  }
-})
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      sourcemap: true,
+      minify: 'terser',
+      chunkSizeWarningLimit: 1024,
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        },
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: {},
+          chunkFileNames: 'js/[name]-[hash].js',
+          assetFileNames: 'assets/[name]-[hash].[ext]',
+          entryFileNames: 'js/[name].[hash].js',
+        },
+      },
+    },
+
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+          modifyVars: {
+            '@primary-color': '#1890ff',
+          },
+        },
+        sass: {},
+      },
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.VITE_APP_ENV),
+      __APP_BASE_API__: JSON.stringify(env.VITE_APP_BASE_API),
+      __APP_TITLE__: JSON.stringify(env.VITE_APP_TITLE),
+    },
+  };
+});
