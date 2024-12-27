818265cd9e68b4fea8ae8bdec73e432457de2a38
huangyafeng <v-huangyafeng1@xiaomi.com>
2024-12-28 03:40:01 +0800

    feat: 提交

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// 引入并配置埋点 SDK
+import * as Sentry from '@sentry/react';
+import { browserTracingIntegration } from "@sentry/react";
+
+Sentry.init({
+  dsn: 'YOUR_DSN',
+  integrations: [browserTracingIntegration()],
+
+  // 设置追踪采样率
+  tracesSampleRate: 1.0,
+});
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
