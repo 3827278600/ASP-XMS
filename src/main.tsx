28735f44dc45680aa283f22f274204e491f6ec45
huangyafeng <v-huangyafeng1@xiaomi.com>
2024-12-28 03:46:20 +0800

    tijiao

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-// 引入并配置埋点 SDK
-import * as Sentry from '@sentry/react';
-import { browserTracingIntegration } from "@sentry/react";
+// // 引入并配置埋点 SDK
+// import * as Sentry from '@sentry/react';
+// import { browserTracingIntegration } from "@sentry/react";
 
-Sentry.init({
-  dsn: 'YOUR_DSN',
-  integrations: [browserTracingIntegration()],
+// Sentry.init({
+//   dsn: 'YOUR_DSN',
+//   integrations: [browserTracingIntegration()],
 
-  // 设置追踪采样率
-  tracesSampleRate: 1.0,
-});
+//   // 设置追踪采样率
+//   tracesSampleRate: 1.0,
+// });
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8000,
+    open: true,
+    host: '0.0.0.0',
+    strictPort: false,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 })
